e818d4aafd6ed4f9a5a645ba318f6ebb3c87508d
Jon Hatfield <jhatfield@fusionalliance.com>
2019-04-26 14:54:38 -0400

    setx PATH
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
          modified:   set_enviromental_vars.cmd

--- set_enviromental_vars.cmd
+++ set_enviromental_vars.cmd
@@ -12,7 +12,7 @@ setx JAVA_HOME "C:\Program Files\Java\jdk1.8.0_latest"
 setx MAVEN_HOME "D:\tools\apache-maven-3.2.5"
 setx WAS_HOME "D:\tools\IBM\WebSphere\85\AppServer"
 
-%MAVEN_HOME%\bin;%WAS_HOME%\bin;
+setx "PATH=%PATH%;%MAVEN_HOME%\bin;%WAS_HOME%\bin"
 
 
 
